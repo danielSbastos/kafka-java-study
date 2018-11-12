db622b4cbc7c1ea79e0d106cd86f0ecc60f1809b
Daniel Bastos <daniel.bastos1710@gmail.com>
2018-11-12 10:41:38 -0200

    Create sample producer

---
+++ src/main/java/com/mapr/examples/ProducerExample.java
@@ -0,0 +1,18 @@
+package com.mapr.examples;
+
+//import com.google.common.io.Resources;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+public class ProducerExample {
+    public static void main(String[] args) throws IOException {
+        KafkaProducer<String, String> producer;
+
+    }
+}
+
