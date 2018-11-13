3291e0ddbaf39fc46faa9bc91f1ef14fdf129758
Daniel Bastos <daniel.bastos1710@gmail.com>
2018-11-13 14:10:07 -0200

    Create functional producer

--- src/main/java/com/mapr/examples/ProducerExample.java
+++ src/main/java/com/mapr/examples/ProducerExample.java
@@ -1,18 +1,24 @@
 package com.mapr.examples;
 
-//import com.google.common.io.Resources;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
 
 public class ProducerExample {
     public static void main(String[] args) throws IOException {
-        KafkaProducer<String, String> producer;
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        for(int i = 0; i < 100; i ++) {
+            System.out.println(i);
+            producer.send(new ProducerRecord<String, String>("daniel", Integer.toString(i), Integer.toString(i)));
+        }
+        producer.close();
     }
 }
-
